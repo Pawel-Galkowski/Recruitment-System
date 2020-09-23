8b90406db7b6aacece37ef6ce05ddb31ce3aa9a2
Pawel-Galkowski <pawel.g-97@o2.pl>
2020-09-23 11:04:48 +0200

    joinjobs #1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# System Rekrutacyjny
- 
+﻿# ExpressApp1
+
+
---
+++ client/src/App.js
@@ -0,0 +1,49 @@
+import React, { Fragment, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Routes from "./components/routing/Routes";
+import Menu from "./components/layout/Menu";
+//Redux
+import { Provider } from "react-redux";
+import store from "./store";
+import { loadUser } from "./actions/auth";
+
+//Stylesheets
+import setAuthToken from "./utils/setAuthToken";
+import "./stylesheet/menuLayout/style.css";
+import "./stylesheet/mainLayout/App.css";
+import "./stylesheet/authLayout/auth.css";
+import "./stylesheet/formsLayout/forms.css";
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <main>
+            <Navbar />
+            <div className="rowLayout">
+              <Menu />
+              <Switch>
+                <Route exact path="/" component={Landing} />
+                <Route component={Routes} />
+              </Switch>
+            </div>
+          </main>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;
